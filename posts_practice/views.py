1ca25a3b2065648104e8f92eb4da53fc1e48b43c
whitecrow <edin.whitecrow@gmail.com>
2023-08-23 17:06:05 +0900

    init~

--- posts_practice/views.py
+++ posts_practice/views.py
@@ -1,24 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 
 # Create your views here.
 def index(request):
-    pass
+    posts = Post.objects.all()
+
+    context = {
+        'posts' : posts,
+    }
+
+    return render(request, 'index.html', context)
 
 def detail(request, id):
-    pass
+    post = Post.object.get(id=id)
+
+    context = {
+        'post' : post,
+    }
+
+    return render(request, 'detail.html', context)
 
 def new(request):
-    pass
+    return render(request, 'new.html')
 
 def create(request):
-    pass
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    post = Post(title=title, content=content)
+    post.save()
+
+    return redirect('posts:detail', id=post.id)
 
 def delete(request, id):
-    pass
+    post = Post.objects.get(id=id)
+    post.delete()
+
+    return redirect('posts:index')
 
 def edit(request, id):
-    pass
+    post = Post.objects.get(id=id)
+
+    context = {
+        'post': post,
+    }
+
+    return render(request, 'edit.html', context)
 
 def update(request, id):
-    pass
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    post = Post.objects.get(id=id)
+    post = Post(title=title, content=content)
+    post.save()
+
+    return redirect('posts:detail', id=post.id)
